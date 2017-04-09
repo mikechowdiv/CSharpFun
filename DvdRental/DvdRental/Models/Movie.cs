56e1dc2b92a37ce408d98cbeefe80b4d5faa2c82
Mike Chow <mikechowdiv@gmail.com>
2017-04-08 21:34:09 -0400

    init

--- DvdRental/DvdRental/Models/Movie.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DvdRental.Models
-{
-    public class Movie
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        
-    }
-}
---
+++ DvdRental1/DvdRental1/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using DvdRental1.Models;
+using System.Web.Mvc;
+
+namespace DvdRental1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            return View(movie);
+        }
+    }
+}
