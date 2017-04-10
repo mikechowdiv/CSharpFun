5fe32274220c17675dc9890a06a09eb6902e58ce
Mike Chow <mikechowdiv@gmail.com>
2017-04-10 11:13:01 -0400

    new changes

---
+++ DvdRental1/DvdRental1/Models/MembershipType.cs
@@ -0,0 +1,11 @@
+﻿namespace DvdRental1.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+
+    }
+}
