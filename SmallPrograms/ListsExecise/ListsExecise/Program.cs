ef9a14495406cc27ba5a87e63426e4d7b65948c6
Mike Chow <mikechowdiv@gmail.com>
2017-06-06 11:42:38 +1000

    update

---
+++ SmallPrograms/ListsExecise/ListsExecise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsExecise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //collect a list of numbers
+            string input;
+            int output;
+
+            List<int> numbers = new List<int>();
+
+            do
+            {
+                Console.WriteLine("enter a number (Q to quit): ");
+                input = Console.ReadLine();
+
+                if(int.TryParse(input, out output))
+                {
+                    numbers.Add(output);
+                }
+
+            } while (input != "Q");
+
+            Console.WriteLine("The numbers entered were: {0}", string.Join(",", numbers));
+
+            int sum = 0;
+            for(int i = 0; i < numbers.Count; i++) //in a list instead of using Lenght we use Count
+            {
+                sum += numbers[i];
+
+            }
+
+            Console.WriteLine("The sum of the numbers entered is : {0}", sum);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
