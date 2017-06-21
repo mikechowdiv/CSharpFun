df3dc19c1d245816ce9cd4c6023a43008bd8816b
Mike Chow <mikechowdiv@gmail.com>
2017-06-21 20:38:03 +1000

    update

---
+++ BankApp/BLL/Factory.cs
@@ -0,0 +1,25 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+   public static class Factory
+    {
+        public static AccountManager Create()
+        {
+            string mode = ConfigurationManager.AppSettings["Mode"].ToString();
+            switch (mode)
+            {
+                case "FreeTest":
+                    return new AccountManager(new FreeAcctTestRepo());
+                default:
+                    throw new Exception("Mode value in add config is not valid");
+            }
+        }
+    }
+}
---
+++ BankApp/Test/FreeAcctTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BLL;
+using Models;
+using Models.Responses;
+
+namespace Test
+{
+    [TestFixture]
+    public class FreeAcctTest
+    {
+        [Test]
+        public void CanLoadFreeAcctTestData()
+        {
+            AccountManager manager = Factory.Create();
+            AcctLookupResponse resp = manager.LookupAcct("");
+
+            Assert.IsNotNull(resp.Account);
+            Assert.IsTrue(resp.Success);
+            Assert.AreEqual("12345", resp.Account.AccountNumber);
+        }
+    }
+}
