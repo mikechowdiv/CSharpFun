238dd71f773adb904da3f85d61a8f060c7f02380
Mike Chow <mikechowdiv@gmail.com>
2017-06-23 20:38:57 +1000

    new

---
+++ SmallPrograms/SimpleCalculater/SimpleCalculater/InputConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleCalculater
+{
+   public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTestInput)
+        {
+            double convertedNumber;
+            if(!double.TryParse(argTestInput, out convertedNumber))
+            {
+                throw new ArgumentException("Expected a numeric value");
+            }
+            return convertedNumber;
+        }
+    }
+}
---
+++ SmallPrograms/SimpleCalculater/SimpleCalculater/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculaterEngine calculaterEngine = new CalculaterEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculaterEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
