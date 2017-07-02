a64b55c769e4a06eb04bf9afbac214407cf8924c
Mike Chow <mikechowdiv@gmail.com>
2017-07-02 15:33:06 +1000

    update

---
+++ DatabaseCRUD/DatabaseCRUD/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseCRUD.Models;
+
+namespace DatabaseCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployees()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var employee = dc.Employees.OrderBy(a => a.FirstName).ToList();
+                return Json(new { data = employee }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
