0983030aadd5019b1cb59915e003363f4c49ca1d
Mike Chow <mikechowdiv@gmail.com>
2017-08-11 22:04:36 +1000

    update

---
+++ SmallPrograms/exercise_ado/Part4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "Select Count(ProductId) from tblProduct";
+                cmd.Connection = conn;
+                conn.Open();
+                int TotalRows = (int)cmd.ExecuteScalar();
+                Console.WriteLine("Total Row = " + TotalRows.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SmallPrograms/exercise_ado/Part4/default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Part4
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select ProductId, Laptops, UnitPrice, Qty from tblProduct", conn);
+                conn.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+        }
+    }
+}
