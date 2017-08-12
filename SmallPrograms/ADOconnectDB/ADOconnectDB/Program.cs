d45e43ccf86dec4f50e6ec084a7e36f8c9e5fc2c
Mike Chow <mikechowdiv@gmail.com>
2017-08-12 12:13:41 +1000

    update

--- SmallPrograms/ADOconnectDB/ADOconnectDB/Program.cs
+++ SmallPrograms/ADOconnectDB/ADOconnectDB/Program.cs
@@ -13,11 +13,43 @@ namespace ADOconnectDB
         {
             try
             {
+                Console.Write("enter employee ID: ");
+                int empid = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("enter employee name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("enter employee salary: ");
+                decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+                string query = "insert into emp values(" + empid + " ,'" + name + " '," + salary + ") ";
+                Console.WriteLine(query);
+
                 string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\workspace\CSharpFun\SmallPrograms\ADOconnectDB\ADOconnectDB\Database1.mdf;Integrated Security=True";
                 SqlConnection conn = new SqlConnection(cs);
 
                 conn.Open();
                 Console.WriteLine("established connection");
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                int result = cmd.ExecuteNonQuery();
+                Console.WriteLine(result + " records inserted in emp table");
+
+                Console.WriteLine("---for retreiving data---");
+                query = "select * from emp";
+                SqlCommand cmd1 = new SqlCommand(query, conn);
+
+                SqlDataReader reader = cmd1.ExecuteReader();
+
+                while (reader.Read() == true)
+                {
+                    int eid = reader.GetInt32(0); //0-column index [empid]
+                    string rm = reader.GetString(1); //1-column index[name]
+                    decimal sal = reader.GetDecimal(2); //2-column index[salary]
+                    Console.WriteLine("{0}\t {1} \t {2}", eid, rm, sal);
+
+                }
+
                 conn.Close();
 
             }
